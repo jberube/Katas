7c4665bd0c5798372ac0442fa48f34c316f3d6ba
Julien Bérubé <julien.berube@gmail.com>
2013-02-02 11:49:54 -0500

    Added a test that chalenges my current implementation, but implemented it the cheapest way possible.

--- Roman Numbers/spec/romanNumbersSpec.js
+++ Roman Numbers/spec/romanNumbersSpec.js
@@ -1,15 +1,17 @@
-describe('Roman Numbers', function() {
-	it('when given 1 returns I', function() {
-		var roman = toRoman(1);
-		expect(roman).toEqual('I');	
+describe('Roman Numbers', function () {
+	it('when given 1 returns I', function () {
+		expect(toRoman(1)).toEqual('I');	
 	});
 
-	it('when given 2 returns II', function() {
+	it('when given 2 returns II', function () {
 		expect(toRoman(2)).toEqual('II');
 	});
 	
 	it('when given 5 returns V', function () {
 		expect(toRoman(5)).toEqual('V');
 	});
+	
+	it('when given 4 returns IV', function () {
+		expect(toRoman(4)).toEqual('IV');		
+	});
 });
-
--- Roman Numbers/src/romanNumbers.js
+++ Roman Numbers/src/romanNumbers.js
@@ -1,8 +1,12 @@
-function toRoman (latin){
-	var roman = '';	
+function toRoman (latin) {
+	var roman = '';
 	while (latin >= 5) {
 		roman += 'V';
 		latin -= 5;
+	}
+	if (latin == 4) {
+		roman += 'IV';
+		latin -= 4;
 	}
 	while (latin >= 1) {
 		roman += 'I';
