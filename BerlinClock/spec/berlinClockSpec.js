011fbf0508d1fa7e1cf692e89a4de40d5a5d62a4
Julien Bérubé <julien.berube@gmail.com>
2013-02-15 15:09:51 -0500

    failing test for the first two rows

--- BerlinClock/spec/berlinClockSpec.js
+++ BerlinClock/spec/berlinClockSpec.js
@@ -15,6 +15,16 @@ describe("The Berlin Clock", function () {
             expect(berlinClock(new Date(2000, 1, 1, 0, 0, 1))).toMatch(/^O/);
 		});
     });
+    
+    describe("top two rows", function () {
+       
+       //The top two rows of lamps are red. These indicate the hours of a day.
+       //In the top row there are 4 red lamps. Every lamp represents 5 hours.
+       //In the lower row of red lamps every lamp represents 1 hour. 
+       it("has 4 lamps either red (R) or off (O)", function () {
+           expect(berlinClock(new Date(0))).toMatch(/^(Y|O)\n(R|O){4}\n(R|O){4}/);
+       });
+    });
 });
 
 function berlinClock ( time ) {
