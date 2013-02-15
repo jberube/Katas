582ec58f7ba0dfa91f93f28db93ab287bfbee34a
Julien Bérubé <julien.berube@gmail.com>
2013-02-15 14:56:47 -0500

    First passing test doing minimal work.

--- BerlinClock/spec/berlinClockSpec.js
+++ BerlinClock/spec/berlinClockSpec.js
@@ -1,11 +1,19 @@
-describe("Berlin Clock", function() {
+describe("The Berlin Clock", function () {
 
-	beforeEach(function() {
+	beforeEach(function () {
 	});
 
-	describe("", function() {
-
-		it("", function() {
+	describe("lamp on top", function () {
+        
+        // On the top of the clock there is a yellow lamp that blinks on/off every two seconds.
+		it("is yellow on even seconds", function () {
+            var time = new Date(2000, 1, 1, 0, 0, 0);
+            
+            expect(berlinClock(time)).toMatch(/^Y/);
 		});
-	});
+    });
 });
+
+function berlinClock ( time ) {
+    return 'Y';
+}
