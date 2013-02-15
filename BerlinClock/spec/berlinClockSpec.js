47d014f6d6cc6bdb570bcccda1bc953d561080c2
Julien Bérubé <julien.berube@gmail.com>
2013-02-15 15:02:01 -0500

    second test, first bit of logic

--- BerlinClock/spec/berlinClockSpec.js
+++ BerlinClock/spec/berlinClockSpec.js
@@ -11,9 +11,18 @@ describe("The Berlin Clock", function () {
             
             expect(berlinClock(time)).toMatch(/^Y/);
 		});
+
+        // On the top of the clock there is a yellow lamp that blinks on/off every two seconds.
+    	it("is off on odd seconds", function () {
+            var time = new Date(2000, 1, 1, 0, 0, 1);
+            
+            expect(berlinClock(time)).toMatch(/^O/);
+		});
     });
 });
 
 function berlinClock ( time ) {
+    if (time.getSeconds() % 2 == 1)
+        return 'O';
     return 'Y';
 }
