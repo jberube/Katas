83e40d76886a963923439e365036d46eaf5977f0
Julien Bérubé <julien.berube@gmail.com>
2013-02-16 00:21:28 -0500

    Added test for second red lamp. More logic, some logic gets repeated, at least in the tests.

--- BerlinClock/spec/berlinClockSpec.js
+++ BerlinClock/spec/berlinClockSpec.js
@@ -5,12 +5,10 @@ describe("The Berlin Clock", function () {
 
 	describe("lamp on top", function () {
         
-        // On the top of the clock there is a yellow lamp that blinks on/off every two seconds.
 		it("is yellow on even seconds", function () {
             expect(berlinClock(new Date(2000, 1, 1, 0, 0, 0))).toMatch(/^Y/);
 		});
 
-        // On the top of the clock there is a yellow lamp that blinks on/off every two seconds.
     	it("is off on odd seconds", function () {
             expect(berlinClock(new Date(2000, 1, 1, 0, 0, 1))).toMatch(/^O/);
 		});
@@ -18,18 +16,21 @@ describe("The Berlin Clock", function () {
     
     describe("top two rows", function () {
        
-        //The top two rows of lamps are red. These indicate the hours of a day.
-        //In the top row there are 4 red lamps. Every lamp represents 5 hours.
-        //In the lower row of red lamps every lamp represents 1 hour. 
         it("each has 4 lamps that can be either red (R) or off (O)", function () {
             expect(berlinClock(new Date(0))).toMatch(/^.*\n(R|O){4}\n(R|O){4}\n/);
         });
         
-        //In the top row there are 4 red lamps. Every lamp represents 5 hours.
         it("first lamp of the first row is red (R) if hour is equal or greater than 5", function () {
             expect(berlinClock(new Date(2000, 1, 1, 5, 0, 0))).toMatch(/^.*\nR/);
+            expect(berlinClock(new Date(2000, 1, 1, 12, 0, 0))).toMatch(/^.*\nR/);
         });
-       
+
+        it("second lamp of the first row is red (R) if hour is equal or greater than 10", function () {
+            expect(berlinClock(new Date(2000, 1, 1, 10, 0, 0))).toMatch(/^.*\n(R|O)R/);
+            expect(berlinClock(new Date(2000, 1, 1, 12, 0, 0))).toMatch(/^.*\n(R|O)R/);
+            expect(berlinClock(new Date(2000, 1, 1, 20, 5, 14))).toMatch(/^.*\n(R|O)R/);
+        });
+
     });
 });
 
@@ -46,13 +47,15 @@ function berlinClock ( time ) {
         berlin += row('Y');
     }
     
-    if (time.getHours() >= 5) {
-        berlin += 'R';
-    } else {
-        berlin += 'O';
+    for (var i = 5; i <= 20; i+=5) {
+        if (time.getHours() >= i) {
+            berlin += 'R';
+        } else {
+            berlin += 'O';
+        }
     }
+    berlin += row('');
 
-    berlin += row('OOO');
     berlin += row('OOOO');
     
     return berlin;
