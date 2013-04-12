1e6f6286f6290a768d1985568fdfaef540d7da09
Julien Bérubé <julien.berube@gmail.com>
2013-04-11 21:26:21 -0400

    Created empty JS project template to ease future Katas setup.

---
+++ js template/spec/templateSpec.js
@@ -0,0 +1,12 @@
+describe("Template", function () {
+
+	beforeEach(function () {
+	});
+
+	describe("the thing", function () {
+        
+		it("is empty right now", function () {
+            expect(null).toBeNull();
+		});
+	});
+});
