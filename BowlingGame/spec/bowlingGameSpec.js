c054afed77c9c20ff67527de6f1f6bb84534fdd2
Julien Bérubé <julien.berube@gmail.com>
2013-04-11 21:42:54 -0400

    Initial commit for the Bowling Game Kata

---
+++ BowlingGame/spec/bowlingGameSpec.js
@@ -0,0 +1,12 @@
+describe("Bowling Game", function () {
+
+	beforeEach(function () {
+	});
+
+	describe("", function () {
+        
+		it("", function () {
+            //expect(berlinClock(new Date(2000, 1, 1, 0, 0, 0))).toMatch(/^Y/);
+		});
+	});
+});
