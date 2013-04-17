af61245a2ee2f819e055d9997cc9f86f374c371c
Julien Bérubé <julien.berube@gmail.com>
2013-04-17 08:39:15 -0400

    Third and fourth frame, both spares. Forcing me to get some structure in the code. Added a scoreAtFrame(frame) method to get the score at a previous frame.

--- BowlingGame/spec/bowlingGameSpec.js
+++ BowlingGame/spec/bowlingGameSpec.js
@@ -1,6 +1,6 @@
 describe("Bowling Game", function () {
 	var bowlingGame;
-	var rolls = [1, 4, 4, 5];
+	var rolls = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0];
 	
 	beforeEach(function () {
 		bowlingGame = BowlingGame();
@@ -21,7 +21,24 @@ describe("Bowling Game", function () {
 			expect(bowlingGame.score()).toEqual(14);
 		});	
 	});
+
 	
+	describe("Third frame, knocked 6 and 4, spare, then 5", function () {
+
+		it("score is 29", function () {
+			makeSomeRolls(8);
+			expect(bowlingGame.scoreAtFrame(3)).toEqual(29);
+		});	
+	});
+
+	describe("Fourth frame, knocked 5 and 5, spare, then strike", function () {
+
+		it("score is 49", function () {
+			makeSomeRolls(10);
+			expect(bowlingGame.scoreAtFrame(4)).toEqual(49);
+		});	
+	});
+
 	function makeSomeRolls(howMuch){
 			for (var i = 0; i < howMuch; i++) {
 				bowlingGame.roll(rolls[i]);
--- BowlingGame/src/bowlingGame.js
+++ BowlingGame/src/bowlingGame.js
@@ -1,11 +1,27 @@
 var BowlingGame = function () {
-	var score = 0;
+	var score = [];
 	return {
 		roll : function (pins) {
-			score += pins;
+			score.push(pins);
 		},
-		score : function () {
-			return score;
+		score : function (rame) {
+			return score.reduce(function(previous, current, index, array) {
+				return previous + current;
+			}, 0);
+		},
+		scoreAtFrame : function(frame) {
+			return score.reduce(function(previous, current, index, array) {
+				if (index < frame * 2) {
+					// spare
+					if (index % 2 == 1) {
+						if (array[index] + array[index - 1] == 10) {
+							return previous + current + array[index + 1];
+						}
+					}
+					return previous + current;
+				}
+				return previous;
+			}, 0);		
 		}
 	};
 };
