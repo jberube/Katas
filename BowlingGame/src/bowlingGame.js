ff7abc33d7ff03b95dc5dee1a1548cbef0554ef4
Julien Bérubé <julien.berube@gmail.com>
2013-04-17 08:45:39 -0400

    refactor: extracted the Spare detection logic and reduced nesting in scoreAtFrame(frame)

--- BowlingGame/src/bowlingGame.js
+++ BowlingGame/src/bowlingGame.js
@@ -1,26 +1,33 @@
 var BowlingGame = function () {
 	var score = [];
+
+	function isSpare (roll) {
+		return (roll % 2 == 1 && score[roll] + score[roll- 1] == 10);
+	}
+
 	return {
 		roll : function (pins) {
 			score.push(pins);
 		},
+		
 		score : function (rame) {
 			return score.reduce(function(previous, current, index, array) {
 				return previous + current;
 			}, 0);
 		},
+		
 		scoreAtFrame : function(frame) {
 			return score.reduce(function(previous, current, index, array) {
-				if (index < frame * 2) {
-					// spare
-					if (index % 2 == 1) {
-						if (array[index] + array[index - 1] == 10) {
-							return previous + current + array[index + 1];
-						}
-					}
-					return previous + current;
+				if (index >= frame * 2) {
+					return previous;
 				}
-				return previous;
+				
+				// spare
+				if (isSpare(index)) {
+					return previous + current + array[index + 1];
+				}
+				
+				return previous + current;
 			}, 0);		
 		}
 	};
